3fe96f014318091e0d6f255955b5092d21f100ba
Pedro Munhoz <pedro.munhoz@gmail.com>
2022-04-17 13:00:16 -0300

    Adds validations to AthleteViewModel

--- src/api/Models/ViewModels/AthleteViewModel.cs
+++ src/api/Models/ViewModels/AthleteViewModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using api.Models.Entities;
 
 namespace api.Models.ViewModels;
@@ -5,18 +6,28 @@ namespace api.Models.ViewModels;
 public class AthleteViewModel
 {
     public string Id { get; set; } = "";
-    public int Number { get; set; }
+
+    [Required]
+    public int? Number { get; set; }
+
+    [Required]
     public string Name { get; set; } = "";
+
     public bool Quarterback { get; set; }
+
     public bool RunningBack { get; set; }
+
     public bool TightEnd { get; set; }
+
     public bool Reciever { get; set; }
 
     public Athlete Map()
     {
+        ArgumentNullException.ThrowIfNull(Number);
+
         return new Athlete
         {
-            Number = Number,
+            Number = Number.Value,
             Name = Name,
             Quarterback = Quarterback,
             RunningBack = RunningBack,

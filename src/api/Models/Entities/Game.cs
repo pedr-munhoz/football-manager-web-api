3254ddefbf9c9263322aeb4e477d823d7dc0ed7d
Pedro Munhoz <pedro.munhoz@gmail.com>
2022-08-01 22:07:56 -0300

    namespace nesting fix

--- src/api/Models/Entities/Game.cs
+++ src/api/Models/Entities/Game.cs
@@ -1,11 +1,10 @@
-namespace api.Models.Entities
+namespace api.Models.Entities;
+
+public class Game
 {
-    public class Game
-    {
-        public long Id { get; set; }
-        public DateTime? Date { get; set; }
-        public string HomeTeam { get; set; } = null!;
-        public string AwayTeam { get; set; } = null!;
-        public string? Location { get; set; }
-    }
+    public long Id { get; set; }
+    public DateTime? Date { get; set; }
+    public string HomeTeam { get; set; } = null!;
+    public string AwayTeam { get; set; } = null!;
+    public string? Location { get; set; }
 }
--- src/api/Models/Results/GameResult.cs
+++ src/api/Models/Results/GameResult.cs
@@ -1,27 +1,26 @@
 using api.Infrastructure.Extensions;
 using api.Models.Entities;
 
-namespace api.Models.Results
+namespace api.Models.Results;
+
+public class GameResult
 {
-    public class GameResult
+    public GameResult()
     {
-        public GameResult()
-        {
-        }
-
-        public GameResult(Game entity)
-        {
-            Id = entity.Id.ToStringId();
-            Date = entity.Date;
-            HomeTeam = entity.HomeTeam;
-            AwayTeam = entity.AwayTeam;
-            Location = entity.Location;
-        }
-
-        public string Id { get; set; } = null!;
-        public DateTime? Date { get; set; }
-        public string HomeTeam { get; set; } = null!;
-        public string AwayTeam { get; set; } = null!;
-        public string? Location { get; set; }
     }
+
+    public GameResult(Game entity)
+    {
+        Id = entity.Id.ToStringId();
+        Date = entity.Date;
+        HomeTeam = entity.HomeTeam;
+        AwayTeam = entity.AwayTeam;
+        Location = entity.Location;
+    }
+
+    public string Id { get; set; } = null!;
+    public DateTime? Date { get; set; }
+    public string HomeTeam { get; set; } = null!;
+    public string AwayTeam { get; set; } = null!;
+    public string? Location { get; set; }
 }

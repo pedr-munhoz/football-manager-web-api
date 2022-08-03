67b84df33475c547be57b03338efcded05a7c3d4
Pedro Munhoz <pedro.munhoz@gmail.com>
2022-08-02 23:09:11 -0300

    migration GameAthleteRelation

---
+++ src/api/Migrations/20220803020844_GameAthleteRelation.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class GameAthleteRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AthleteGame",
+                columns: table => new
+                {
+                    AthletesId = table.Column<long>(type: "bigint", nullable: false),
+                    GamesId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AthleteGame", x => new { x.AthletesId, x.GamesId });
+                    table.ForeignKey(
+                        name: "FK_AthleteGame_Athletes_AthletesId",
+                        column: x => x.AthletesId,
+                        principalTable: "Athletes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AthleteGame_Games_GamesId",
+                        column: x => x.GamesId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AthleteGame_GamesId",
+                table: "AthleteGame",
+                column: "GamesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AthleteGame");
+        }
+    }
+}
